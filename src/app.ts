95bab843cc14d4e314ef20db9611d656aefc5df8
Jan Scheidegger <scheideggerj@gmail.com>
2016-04-07 11:40:27 +0200

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import {Component} from 'angular2/core'
+import {NameComponent} from './name-component'
 
 @Component({
     selector: 'jazoon-app',
     template: `
-        <h1>hello jazoon</h1>
-    `
+        <h1>hello {{name}}</h1>
+        <input type="text" [(ngModel)]="myName">
+        <name-component  [(name)]="myName"></name-component>
+    `,
+    directives: [NameComponent]
 })
-export class App {}
+export class App {
+    myName:string;
+    isDisabled: boolean = false;
+    foos: string[];
+    constructor() {
+        this.foos = ['rob', 'sam', 'carry'];
+        this.myName = 'Jan'
+    }
+    nameChanged(name) {
+        this.myName = name;
+    }
+}
